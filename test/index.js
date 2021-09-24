5db0c6e5a94e397fb3ec55893f60dfa24e10de71
Jessica Nahulan <jess.n169@hotmail.com>
2021-09-23 20:06:54 -0400

    Revert "feat(parseArgs): set positionals and handle withValue with multiples options"
    This reverts commit 448655143c13b054adccdad1a4433dd23f83c15e.

--- test/index.js
+++ test/index.js
@@ -3,8 +3,6 @@
 const test = require('tape')
 const {parseArgs} = require('../index.js')
 
-//Test results are as we expect
-
 test('Everything after a bare `--` is considered a positional argument', function (t) {
   const passedArgs = ['--', 'barepositionals', 'mopositionals']
   const expected = { args: {}, values: {}, positionals: ['barepositionals', 'mopositionals'] }
@@ -17,7 +15,7 @@ test('Everything after a bare `--` is considered a positional argument', functio
 
 test('args are true', function (t) {
   const passedArgs = ['--foo', '--bar']
-  const expected = { args: { foo: true, bar: true}, values: {foo: [undefined], bar: [undefined]}, positionals: [] }
+  const expected = { args: { foo: true, bar: true}, values: {}, positionals: [] }
   const args = parseArgs(passedArgs)
 
   t.deepEqual(args, expected, 'args are true')
@@ -25,20 +23,10 @@ test('args are true', function (t) {
   t.end()
 })
 
-test('arg is true and positional is identified', function (t) {
-  const passedArgs = ['--foo=a', '--foo', 'b']
-  const expected = { args: { foo: true}, values: { foo: [undefined]}, positionals: ['b'] }
-  const args = parseArgs(passedArgs)
-
-  t.deepEqual(args, expected, 'arg is true and positional is identified')
-
-  t.end()
-})
-
 test('args equals are passed "withValue"', function (t) {
   const passedArgs = ['--so=wat']
-  const passedOptions = { withValue: ['so'] }
-  const expected = { args: { so: true}, values: { so: ["wat"]}, positionals: [] }
+  const passedOptions = { withValue: true }
+  const expected = { args: { so: true}, values: { so: "wat"}, positionals: [] }
   const args = parseArgs(passedArgs, passedOptions)
 
   t.deepEqual(args, expected, 'arg value is passed')
@@ -46,45 +34,3 @@ test('args equals are passed "withValue"', function (t) {
   t.end()
 })
 
-test('same arg is passed twice "withValue" and last value is recorded', function (t) {
-  const passedArgs = ['--foo=a', '--foo', 'b']
-  const passedOptions = { withValue: ['foo'] }
-  const expected = { args: { foo: true}, values: { foo: ['b']}, positionals: [] }
-  const args = parseArgs(passedArgs, passedOptions)
-
-  t.deepEqual(args, expected, 'last arg value is passed')
-
-  t.end()
-})
-
-test('args are passed "withValue" and "multiples"', function (t) {
-  const passedArgs = ['--foo=a', '--foo', 'b']
-  const passedOptions = { withValue: ['foo'], multiples: ['foo'] }
-  const expected = { args: { foo: true}, values: { foo: ['a', 'b']}, positionals: [] }
-  const args = parseArgs(passedArgs, passedOptions)
-
-  t.deepEqual(args, expected, 'both arg values are passed')
-
-  t.end()
-})
-
-
-//Test bad inputs
-
-test('boolean passed to "withValue" option', function (t) {
-  const passedArgs = ['--so=wat']
-  const passedOptions = { withValue: true }
-
-  t.throws(function() { parseArgs(passedArgs, passedOptions) });
-
-  t.end()
-})
-
-test('string passed to "withValue" option', function (t) {
-  const passedArgs = ['--so=wat']
-  const passedOptions = { withValue: 'so' }
-
-  t.throws(function() { parseArgs(passedArgs, passedOptions) });
-
-  t.end()
-})
