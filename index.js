a35c9b711a30be50be19912ab460a69937e60c7b
Benjamin E. Coe <bencoe@google.com>
2022-01-17 13:55:33 +0000

    refactor: use primordials (#37)

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 'use strict';
 
+const {
+  ArrayIsArray,
+  ArrayPrototypeConcat,
+  ArrayPrototypeIncludes,
+  ArrayPrototypeSlice,
+  ArrayPrototypePush,
+  StringPrototypeCharAt,
+  StringPrototypeIncludes,
+  StringPrototypeSlice,
+  StringPrototypeSplit,
+  StringPrototypeStartsWith,
+} = require('./primordials');
+
 function getMainArgs() {
   // This function is a placeholder for proposed process.mainArgs.
   // Work out where to slice process.argv for user supplied arguments.
@@ -18,18 +31,20 @@ function getMainArgs() {
   // (Not included in tests as hopefully temporary example.)
   /* c8 ignore next 3 */
   if (process.versions && process.versions.electron && !process.defaultApp) {
-    return process.argv.slice(1);
+    return ArrayPrototypeSlice(process.argv, 1);
   }
 
   // Check node options for scenarios where user CLI args follow executable.
   const execArgv = process.execArgv;
-  if (execArgv.includes('-e') || execArgv.includes('--eval') ||
-      execArgv.includes('-p') || execArgv.includes('--print')) {
-    return process.argv.slice(1);
+  if (StringPrototypeIncludes(execArgv, '-e') ||
+      StringPrototypeIncludes(execArgv, '--eval') ||
+      StringPrototypeIncludes(execArgv, '-p') ||
+      StringPrototypeIncludes(execArgv, '--print')) {
+    return ArrayPrototypeSlice(process.argv, 1);
   }
 
   // Normally first two arguments are executable and script, then CLI arguments
-  return process.argv.slice(2);
+  return ArrayPrototypeSlice(process.argv, 2);
 }
 
 const parseArgs = (
@@ -39,7 +54,7 @@ const parseArgs = (
   if (typeof options !== 'object' || options === null) {
     throw new Error('Whoops!');
   }
-  if (options.withValue !== undefined && !Array.isArray(options.withValue)) {
+  if (options.withValue !== undefined && !ArrayIsArray(options.withValue)) {
     throw new Error('Whoops! options.withValue should be an array.');
   }
 
@@ -53,37 +68,44 @@ const parseArgs = (
   while (pos < argv.length) {
     let arg = argv[pos];
 
-    if (arg.startsWith('-')) {
+    if (StringPrototypeStartsWith(arg, '-')) {
       // Everything after a bare '--' is considered a positional argument
       // and is returned verbatim
       if (arg === '--') {
-        result.positionals.push(...argv.slice(++pos));
+        result.positionals = ArrayPrototypeConcat(
+          result.positionals,
+          ArrayPrototypeSlice(argv, ++pos)
+        );
         return result;
-      } else if (arg.charAt(1) !== '-') { // Look for shortcodes: -fXzy
+      } else if (
+        StringPrototypeCharAt(arg, 1) !== '-'
+      ) { // Look for shortcodes: -fXzy
         throw new Error('What are we doing with shortcodes!?!');
       }
 
-      arg = arg.slice(2); // remove leading --
+      arg = StringPrototypeSlice(arg, 2); // remove leading --
 
-      if (arg.includes('=')) {
+      if (StringPrototypeIncludes(arg, '=')) {
         // withValue equals(=) case
-        const argParts = arg.split('=');
+        const argParts = StringPrototypeSplit(arg, '=');
 
         result.flags[argParts[0]] = true;
         // If withValue option is specified, take 2nd part after '=' as value,
         // else set value as undefined
         const val = options.withValue &&
-          options.withValue.includes(argParts[0]) ?
+          ArrayPrototypeIncludes(options.withValue, argParts[0]) ?
           argParts[1] : undefined;
         // Append value to previous values array for case of multiples
         // option, else add to empty array
-        result.values[argParts[0]] = [].concat(
-          options.multiples &&
-            options.multiples.includes(argParts[0]) &&
+        result.values[argParts[0]] = ArrayPrototypeConcat([],
+                                                          options.multiples &&
+            ArrayPrototypeIncludes(options.multiples, argParts[0]) &&
             result.values[argParts[0]] || [],
-          val,
+                                                          val,
         );
-      } else if (pos + 1 < argv.length && !argv[pos + 1].startsWith('-')) {
+      } else if (pos + 1 < argv.length &&
+        !StringPrototypeStartsWith(argv[pos + 1], '-')
+      ) {
         // withValue option should also support setting values when '=
         // isn't used ie. both --foo=b and --foo b should work
 
@@ -92,17 +114,19 @@ const parseArgs = (
         // value and then increment pos so that we don't re-evaluate that
         // arg, else set value as undefined ie. --foo b --bar c, after setting
         // b as the value for foo, evaluate --bar next and skip 'b'
-        const val = options.withValue && options.withValue.includes(arg) ?
-          argv[++pos] :
+        const val = options.withValue &&
+          ArrayPrototypeIncludes(options.withValue, arg) ? argv[++pos] :
           undefined;
         // Append value to previous values array for case of multiples
         // option, else add to empty array
-        result.values[arg] = [].concat(
-          options.multiples && options.multiples.includes(arg) &&
-            result.values[arg] ?
-            result.values[arg] :
-            [],
-          val);
+        result.values[arg] = ArrayPrototypeConcat(
+          [],
+          options.multiples &&
+          ArrayPrototypeIncludes(options.multiples, arg) &&
+          result.values[arg] ||
+          [],
+          val
+        );
       } else {
         // Cases when an arg is specified without a value, example
         // '--foo --bar' <- 'foo' and 'bar' flags should be set to true and
@@ -110,18 +134,19 @@ const parseArgs = (
         result.flags[arg] = true;
         // Append undefined to previous values array for case of
         // multiples option, else add to empty array
-        result.values[arg] = [].concat(
-          options.multiples && options.multiples.includes(arg) &&
-            result.values[arg] ?
-            result.values[arg] :
-            [],
+        result.values[arg] = ArrayPrototypeConcat(
+          [],
+          options.multiples &&
+          ArrayPrototypeIncludes(options.multiples, arg) &&
+          result.values[arg] ||
+          [],
           undefined
         );
       }
 
     } else {
       // Arguements without a dash prefix are considered "positional"
-      result.positionals.push(arg);
+      ArrayPrototypePush(result.positionals, arg);
     }
 
     pos++;
--- test/index.js
+++ test/index.js
@@ -1,72 +1,73 @@
 'use strict';
+/* eslint max-len: 0 */
 
-const test = require('tape')
-const {parseArgs} = require('../index.js')
+const test = require('tape');
+const { parseArgs } = require('../index.js');
 
-//Test results are as we expect
+// Test results are as we expect
 
-test('Everything after a bare `--` is considered a positional argument', function (t) {
-  const passedArgs = ['--', 'barepositionals', 'mopositionals']
-  const expected = { flags: {}, values: {}, positionals: ['barepositionals', 'mopositionals'] }
-  const args = parseArgs(passedArgs)
+test('Everything after a bare `--` is considered a positional argument', function(t) {
+  const passedArgs = ['--', 'barepositionals', 'mopositionals'];
+  const expected = { flags: {}, values: {}, positionals: ['barepositionals', 'mopositionals'] };
+  const args = parseArgs(passedArgs);
 
-  t.deepEqual(args, expected, 'testing bare positionals')
+  t.deepEqual(args, expected, 'testing bare positionals');
 
-  t.end()
-})
+  t.end();
+});
 
-test('args are true', function (t) {
-  const passedArgs = ['--foo', '--bar']
-  const expected = { flags: { foo: true, bar: true}, values: {foo: [undefined], bar: [undefined]}, positionals: [] }
-  const args = parseArgs(passedArgs)
+test('args are true', function(t) {
+  const passedArgs = ['--foo', '--bar'];
+  const expected = { flags: { foo: true, bar: true }, values: { foo: [undefined], bar: [undefined] }, positionals: [] };
+  const args = parseArgs(passedArgs);
 
-  t.deepEqual(args, expected, 'args are true')
+  t.deepEqual(args, expected, 'args are true');
 
-  t.end()
-})
+  t.end();
+});
 
-test('arg is true and positional is identified', function (t) {
-  const passedArgs = ['--foo=a', '--foo', 'b']
-  const expected = { flags: { foo: true}, values: { foo: [undefined]}, positionals: ['b'] }
-  const args = parseArgs(passedArgs)
+test('arg is true and positional is identified', function(t) {
+  const passedArgs = ['--foo=a', '--foo', 'b'];
+  const expected = { flags: { foo: true }, values: { foo: [undefined] }, positionals: ['b'] };
+  const args = parseArgs(passedArgs);
 
-  t.deepEqual(args, expected, 'arg is true and positional is identified')
+  t.deepEqual(args, expected, 'arg is true and positional is identified');
 
-  t.end()
-})
+  t.end();
+});
 
-test('args equals are passed "withValue"', function (t) {
-  const passedArgs = ['--so=wat']
-  const passedOptions = { withValue: ['so'] }
-  const expected = { flags: { so: true}, values: { so: ["wat"]}, positionals: [] }
-  const args = parseArgs(passedArgs, passedOptions)
+test('args equals are passed "withValue"', function(t) {
+  const passedArgs = ['--so=wat'];
+  const passedOptions = { withValue: ['so'] };
+  const expected = { flags: { so: true }, values: { so: ['wat'] }, positionals: [] };
+  const args = parseArgs(passedArgs, passedOptions);
 
-  t.deepEqual(args, expected, 'arg value is passed')
+  t.deepEqual(args, expected, 'arg value is passed');
 
-  t.end()
-})
+  t.end();
+});
 
-test('same arg is passed twice "withValue" and last value is recorded', function (t) {
-  const passedArgs = ['--foo=a', '--foo', 'b']
-  const passedOptions = { withValue: ['foo'] }
-  const expected = { flags: { foo: true}, values: { foo: ['b']}, positionals: [] }
-  const args = parseArgs(passedArgs, passedOptions)
+test('same arg is passed twice "withValue" and last value is recorded', function(t) {
+  const passedArgs = ['--foo=a', '--foo', 'b'];
+  const passedOptions = { withValue: ['foo'] };
+  const expected = { flags: { foo: true }, values: { foo: ['b'] }, positionals: [] };
+  const args = parseArgs(passedArgs, passedOptions);
 
-  t.deepEqual(args, expected, 'last arg value is passed')
+  t.deepEqual(args, expected, 'last arg value is passed');
 
-  t.end()
-})
+  t.end();
+});
 
-test('args are passed "withValue" and "multiples"', function (t) {
-  const passedArgs = ['--foo=a', '--foo', 'b']
-  const passedOptions = { withValue: ['foo'], multiples: ['foo'] }
-  const expected = { flags: { foo: true}, values: { foo: ['a', 'b']}, positionals: [] }
-  const args = parseArgs(passedArgs, passedOptions)
+test('args are passed "withValue" and "multiples"', function(t) {
+  const passedArgs = ['--foo=a', '--foo', 'b'];
+  const passedOptions = { withValue: ['foo'], multiples: ['foo'] };
+  const expected = { flags: { foo: true }, values: { foo: ['a', 'b'] }, positionals: [] };
+  const args = parseArgs(passedArgs, passedOptions);
 
-  t.deepEqual(args, expected, 'both arg values are passed')
+  t.deepEqual(args, expected, 'both arg values are passed');
 
-  t.end()
-})
+  t.end();
+});
 
 test('correct default args when use node -p', function(t) {
   const holdArgv = process.argv;
@@ -171,20 +172,20 @@ test('excess leading dashes on options are retained', function(t) {
 
 // Test bad inputs
 
-test('boolean passed to "withValue" option', function (t) {
-  const passedArgs = ['--so=wat']
-  const passedOptions = { withValue: true }
+test('boolean passed to "withValue" option', function(t) {
+  const passedArgs = ['--so=wat'];
+  const passedOptions = { withValue: true };
 
-  t.throws(function() { parseArgs(passedArgs, passedOptions) });
+  t.throws(function() { parseArgs(passedArgs, passedOptions); });
 
-  t.end()
-})
+  t.end();
+});
 
-test('string passed to "withValue" option', function (t) {
-  const passedArgs = ['--so=wat']
-  const passedOptions = { withValue: 'so' }
+test('string passed to "withValue" option', function(t) {
+  const passedArgs = ['--so=wat'];
+  const passedOptions = { withValue: 'so' };
 
-  t.throws(function() { parseArgs(passedArgs, passedOptions) });
+  t.throws(function() { parseArgs(passedArgs, passedOptions); });
 
-  t.end()
-})
+  t.end();
+});
