512afe5fa0d66e77de5cf9e7cebb217d00db1788
John Gee <john@ruru.gen.nz>
2022-03-02 15:24:52 +1300

    Update signature for running new tests to arrow functions

--- test/is-lone-short-option.js
+++ test/is-lone-short-option.js
@@ -4,42 +4,42 @@
 const test = require('tape');
 const { isLoneShortOption } = require('../util.js');
 
-test('isLoneShortOption: when passed short option then returns true', function(t) {
+test('isLoneShortOption: when passed short option then returns true', (t) => {
   t.true(isLoneShortOption('-s'));
   t.end();
 });
 
-test('isLoneShortOption: when passed short option group then returns false', function(t) {
+test('isLoneShortOption: when passed short option group then returns false', (t) => {
   t.false(isLoneShortOption('-abc'));
   t.end();
 });
 
-test('isLoneShortOption: when passed long option then returns false', function(t) {
+test('isLoneShortOption: when passed long option then returns false', (t) => {
   t.false(isLoneShortOption('--foo'));
   t.end();
 });
 
-test('isLoneShortOption: when passed long option with value then returns false', function(t) {
+test('isLoneShortOption: when passed long option with value then returns false', (t) => {
   t.false(isLoneShortOption('--foo=bar'));
   t.end();
 });
 
-test('isLoneShortOption: when passed empty string then returns false', function(t) {
+test('isLoneShortOption: when passed empty string then returns false', (t) => {
   t.false(isLoneShortOption(''));
   t.end();
 });
 
-test('isLoneShortOption: when passed plain text then returns false', function(t) {
+test('isLoneShortOption: when passed plain text then returns false', (t) => {
   t.false(isLoneShortOption('foo'));
   t.end();
 });
 
-test('isLoneShortOption: when passed single dash then returns false', function(t) {
+test('isLoneShortOption: when passed single dash then returns false', (t) => {
   t.false(isLoneShortOption('-'));
   t.end();
 });
 
-test('isLoneShortOption: when passed double dash then returns false', function(t) {
+test('isLoneShortOption: when passed double dash then returns false', (t) => {
   t.false(isLoneShortOption('--'));
   t.end();
 });
--- test/is-long-option.js
+++ test/is-long-option.js
@@ -4,48 +4,48 @@
 const test = require('tape');
 const { isLongOption } = require('../util.js');
 
-test('isLongOption: when passed short option then returns false', function(t) {
+test('isLongOption: when passed short option then returns false', (t) => {
   t.false(isLongOption('-s'));
   t.end();
 });
 
-test('isLongOption: when passed short option group then returns false', function(t) {
+test('isLongOption: when passed short option group then returns false', (t) => {
   t.false(isLongOption('-abc'));
   t.end();
 });
 
-test('isLongOption: when passed long option then returns true', function(t) {
+test('isLongOption: when passed long option then returns true', (t) => {
   t.true(isLongOption('--foo'));
   t.end();
 });
 
-test('isLongOption: when passed long option with value then returns true', function(t) {
+test('isLongOption: when passed long option with value then returns true', (t) => {
   t.true(isLongOption('--foo=bar'));
   t.end();
 });
 
-test('isLongOption: when passed empty string then returns false', function(t) {
+test('isLongOption: when passed empty string then returns false', (t) => {
   t.false(isLongOption(''));
   t.end();
 });
 
-test('isLongOption: when passed plain text then returns false', function(t) {
+test('isLongOption: when passed plain text then returns false', (t) => {
   t.false(isLongOption('foo'));
   t.end();
 });
 
-test('isLongOption: when passed single dash then returns false', function(t) {
+test('isLongOption: when passed single dash then returns false', (t) => {
   t.false(isLongOption('-'));
   t.end();
 });
 
-test('isLongOption: when passed double dash then returns false', function(t) {
+test('isLongOption: when passed double dash then returns false', (t) => {
   t.false(isLongOption('--'));
   t.end();
 });
 
 // This is a bit bogus, but simple consistent behaviour: long option follows double dash.
-test('isLongOption: when passed arg starting with triple dash then returns true', function(t) {
+test('isLongOption: when passed arg starting with triple dash then returns true', (t) => {
   t.true(isLongOption('---foo'));
   t.end();
 });
