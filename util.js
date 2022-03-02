c8c9e0c18a913b2065773ff9115924b637ff90dd
John Gee <john@ruru.gen.nz>
2022-03-02 13:27:06 +1300

    Add isLongOption

--- util.js
+++ util.js
@@ -11,14 +11,14 @@ const {
 // and just tested implicitly via API).
 
 /**
-   * Determines if the argument may be used as an option value.
-   * NB: We are choosing not to accept option-ish arguments.
-   * @example
-   * isPossibleOptionValue('V']) // returns true
-   * isPossibleOptionValue('-v') // returns false
-   * isPossibleOptionValue('--foo') // returns false
-   * isPossibleOptionValue(undefined) // returns false
-   */
+ * Determines if the argument may be used as an option value.
+ * NB: We are choosing not to accept option-ish arguments.
+ * @example
+ * isPossibleOptionValue('V']) // returns true
+ * isPossibleOptionValue('-v') // returns false
+ * isPossibleOptionValue('--foo') // returns false
+ * isPossibleOptionValue(undefined) // returns false
+ */
 function isPossibleOptionValue(value) {
   if (value === undefined) return false;
   if (value === '-') return true; // e.g. representing stdin/stdout for file
@@ -32,16 +32,28 @@ function isPossibleOptionValue(value) {
 }
 
 /**
-   * Determines if `arg` is a just a short option.
-   * @example '-f'
-   */
+ * Determines if `arg` is a just a short option.
+ * @example '-f'
+ */
 function isLoneShortOption(arg) {
   return arg.length === 2 &&
     StringPrototypeCharAt(arg, 0) === '-' &&
     StringPrototypeCharAt(arg, 1) !== '-';
 }
 
+/**
+ * Determines if `arg` is a long option, which may have a trailing value.
+ * @example
+ * isLongOption('-a) // returns false
+ * isLongOption('--foo) // returns true
+ * isLongOption('--foo=bar) // returns true
+ */
+function isLongOption(arg) {
+  return arg.length > 2 && StringPrototypeStartsWith(arg, '--');
+}
+
 module.exports = {
+  isLongOption,
   isLoneShortOption,
   isPossibleOptionValue
 };
