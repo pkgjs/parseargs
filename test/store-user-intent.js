cf48248010d0cc789b5d385101e987a90bbaa3fa
John Gee <john@ruru.gen.nz>
2022-03-04 17:30:00 +1300

    Add tests for failure duck typing

---
+++ test/store-user-intent.js
@@ -0,0 +1,52 @@
+'use strict';
+/* eslint max-len: 0 */
+
+const test = require('tape');
+const { parseArgs } = require('../index.js');
+
+
+// Rationale
+//
+// John Gee:
+// Looks like a boolean option, stored like a flag. Looks like a string option, stored like a string option.
+// No loss of information. No new pattern to learn in result.
+//
+// Jordan Harband: In other words, the way they're stored matches the intention of the user,
+// not the configurer, which will ensure the configurer can most accurately respond to the
+// user's intentions.
+
+test('when use string short option used as boolean then result as if boolean', (t) => {
+  const passedArgs = ['-o'];
+  const stringOptions = { opt: { short: 'o', type: 'string' } };
+  const booleanOptions = { opt: { short: 'o', type: 'boolean' } };
+
+  const stringConfigResult = parseArgs({ args: passedArgs, options: stringOptions, strict: false });
+  const booleanConfigResult = parseArgs({ args: passedArgs, options: booleanOptions, strict: false });
+
+  t.deepEqual(stringConfigResult, booleanConfigResult);
+  t.end();
+});
+
+test('when use string long option used as boolean then result as if boolean', (t) => {
+  const passedArgs = ['--opt'];
+  const stringOptions = { opt: { short: 'o', type: 'string' } };
+  const booleanOptions = { opt: { short: 'o', type: 'boolean' } };
+
+  const stringConfigResult = parseArgs({ args: passedArgs, options: stringOptions, strict: false });
+  const booleanConfigResult = parseArgs({ args: passedArgs, options: booleanOptions, strict: false });
+
+  t.deepEqual(stringConfigResult, booleanConfigResult);
+  t.end();
+});
+
+test('when use boolean long option used as string then result as if string', (t) => {
+  const passedArgs = ['--bool=OOPS'];
+  const stringOptions = { bool: { short: 'b', type: 'string' } };
+  const booleanOptions = { bool: { short: 'b', type: 'boolean' } };
+
+  const stringConfigResult = parseArgs({ args: passedArgs, options: stringOptions, strict: false });
+  const booleanConfigResult = parseArgs({ args: passedArgs, options: booleanOptions, strict: false });
+
+  t.deepEqual(booleanConfigResult, stringConfigResult);
+  t.end();
+});
