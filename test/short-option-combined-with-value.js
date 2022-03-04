3a7ea3c1bfdfb972c791196c29d136a4536a9f69
John Gee <john@ruru.gen.nz>
2022-03-04 13:31:14 +1300

    Add tests for combining short and value

---
+++ test/short-option-combined-with-value.js
@@ -0,0 +1,90 @@
+'use strict';
+/* eslint max-len: 0 */
+
+const test = require('tape');
+const { parseArgs } = require('../index.js');
+
+test('when combine string short with plain text then parsed as value', (t) => {
+  const passedArgs = ['-aHELLO'];
+  const passedOptions = { alpha: { short: 'a', type: 'string' } };
+
+  const result = parseArgs({ args: passedArgs, options: passedOptions });
+  const expected = { flags: { alpha: true }, values: { alpha: 'HELLO' }, positionals: [] };
+
+  t.deepEqual(result, expected);
+
+  t.end();
+});
+
+test('when combine low-config string short with plain text then parsed as value', (t) => {
+  const passedArgs = ['-aHELLO'];
+  const passedOptions = { a: { type: 'string' } };
+
+  const result = parseArgs({ args: passedArgs, options: passedOptions });
+  const expected = { flags: { a: true }, values: { a: 'HELLO' }, positionals: [] };
+
+  t.deepEqual(result, expected);
+
+  t.end();
+});
+
+test('when combine string short with value like short option then parsed as value', (t) => {
+  const passedArgs = ['-a-b'];
+  const passedOptions = { alpha: { short: 'a', type: 'string' } };
+
+  const result = parseArgs({ args: passedArgs, options: passedOptions });
+  const expected = { flags: { alpha: true }, values: { alpha: '-b' }, positionals: [] };
+
+  t.deepEqual(result, expected);
+
+  t.end();
+});
+
+test('when combine string short with value like long option then parsed as value', (t) => {
+  const passedArgs = ['-a--bar'];
+  const passedOptions = { alpha: { short: 'a', type: 'string' } };
+
+  const result = parseArgs({ args: passedArgs, options: passedOptions });
+  const expected = { flags: { alpha: true }, values: { alpha: '--bar' }, positionals: [] };
+
+  t.deepEqual(result, expected);
+
+  t.end();
+});
+
+test('when combine string short with value like negative number then parsed as value', (t) => {
+  const passedArgs = ['-a-5'];
+  const passedOptions = { alpha: { short: 'a', type: 'string' } };
+
+  const result = parseArgs({ args: passedArgs, options: passedOptions });
+  const expected = { flags: { alpha: true }, values: { alpha: '-5' }, positionals: [] };
+
+  t.deepEqual(result, expected);
+
+  t.end();
+});
+
+
+test('when combine string short with value which matches configured flag then parsed as value', (t) => {
+  const passedArgs = ['-af'];
+  const passedOptions = { alpha: { short: 'a', type: 'string' }, file: { short: 'f' } };
+
+  const result = parseArgs({ args: passedArgs, options: passedOptions });
+  const expected = { flags: { alpha: true }, values: { alpha: 'f' }, positionals: [] };
+
+  t.deepEqual(result, expected);
+
+  t.end();
+});
+
+test('when combine string short with value including equals then parsed with equals in value', (t) => {
+  const passedArgs = ['-a=5'];
+  const passedOptions = { alpha: { short: 'a', type: 'string' } };
+
+  const result = parseArgs({ args: passedArgs, options: passedOptions });
+  const expected = { flags: { alpha: true }, values: { alpha: '=5' }, positionals: [] };
+
+  t.deepEqual(result, expected);
+
+  t.end();
+});
