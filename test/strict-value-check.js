6439e9923ab9205bdef16c63e8b19a8a77b5af2c
John Gee <john@ruru.gen.nz>
2022-04-15 21:46:46 +1200

    Refactor tests and fix for new short message

--- test/strict-value-check.js
+++ test/strict-value-check.js
@@ -5,91 +5,101 @@ const test = require('tape');
 const { parseArgs } = require('../index.js');
 
 test('strict: when candidate option value is plain text then does not throw', (t) => {
-  const passedArgs = ['--with', 'abc'];
-  const knownOptions = { with: { type: 'string' } };
+  const args = ['--with', 'abc'];
+  const options = { with: { type: 'string' } };
 
   t.doesNotThrow(() => {
-    parseArgs({ args: passedArgs, options: knownOptions, strict: true });
+    parseArgs({ args, options, strict: true });
   });
   t.end();
 });
 
 test("strict: when candidate option value is '-' then does not throw", (t) => {
-  const passedArgs = ['--with', '-'];
-  const knownOptions = { with: { type: 'string' } };
+  const args = ['--with', '-'];
+  const options = { with: { type: 'string' } };
 
   t.doesNotThrow(() => {
-    parseArgs({ args: passedArgs, options: knownOptions, strict: true });
+    parseArgs({ args, options, strict: true });
   });
   t.end();
 });
 
 test("strict: when candidate option value is '--' then throws", (t) => {
-  const passedArgs = ['--with', '--'];
-  const knownOptions = { with: { type: 'string' } };
+  const args = ['--with', '--'];
+  const options = { with: { type: 'string' } };
 
   t.throws(() => {
-    parseArgs({ args: passedArgs, options: knownOptions, strict: true });
+    parseArgs({ args, options, strict: true });
   });
   t.end();
 });
 
 test('strict: when candidate option value is short option then throws', (t) => {
-  const passedArgs = ['--with', '-a'];
-  const knownOptions = { with: { type: 'string' } };
+  const args = ['--with', '-a'];
+  const options = { with: { type: 'string' } };
 
   t.throws(() => {
-    parseArgs({ args: passedArgs, options: knownOptions, strict: true });
+    parseArgs({ args, options, strict: true });
   });
   t.end();
 });
 
 test('strict: when candidate option value is short option digit then throws', (t) => {
   const passedArgs = ['--with', '-1'];
-  const knownOptions = { with: { type: 'string' } };
+  const options = { with: { type: 'string' } };
 
   t.throws(() => {
-    parseArgs({ args: passedArgs, options: knownOptions, strict: true });
+    parseArgs({ args: passedArgs, options, strict: true });
   });
   t.end();
 });
 
 test('strict: when candidate option value is long option then throws', (t) => {
   const passedArgs = ['--with', '--foo'];
-  const knownOptions = { with: { type: 'string' } };
+  const options = { with: { type: 'string' } };
 
   t.throws(() => {
-    parseArgs({ args: passedArgs, options: knownOptions, strict: true });
+    parseArgs({ args: passedArgs, options, strict: true });
   });
   t.end();
 });
 
 test('strict: when short option and suspect value then throws with short option in error message', (t) => {
   const passedArgs = ['-w', '--foo'];
-  const knownOptions = { with: { type: 'string', short: 'w' } };
+  const options = { with: { type: 'string', short: 'w' } };
 
   t.throws(() => {
-    parseArgs({ args: passedArgs, options: knownOptions, strict: true });
+    parseArgs({ args: passedArgs, options, strict: true });
   }, /for '-w'/);
   t.end();
 });
 
 test('strict: when long option and suspect value then throws with long option in error message', (t) => {
   const passedArgs = ['--with', '--foo'];
-  const knownOptions = { with: { type: 'string' } };
+  const options = { with: { type: 'string' } };
 
   t.throws(() => {
-    parseArgs({ args: passedArgs, options: knownOptions, strict: true });
+    parseArgs({ args: passedArgs, options, strict: true });
   }, /for '--with'/);
+  t.end();
+});
+
+test('strict: when short option and suspect value then throws with whole expected message', (t) => {
+  const passedArgs = ['-w', '--foo'];
+  const options = { with: { type: 'string', short: 'w' } };
+
+  t.throws(() => {
+    parseArgs({ args: passedArgs, options, strict: true });
+  }, /Did you forget to specify the option argument for '-w'\?\nTo specify an option argument starting with a dash use '--with=-XYZ' or '-w-XYZ'\./);
   t.end();
 });
 
 test('strict: when long option and suspect value then throws with whole expected message', (t) => {
   const passedArgs = ['--with', '--foo'];
-  const knownOptions = { with: { type: 'string' } };
+  const options = { with: { type: 'string', short: 'w' } };
 
   t.throws(() => {
-    parseArgs({ args: passedArgs, options: knownOptions, strict: true });
-  }, /Did you forget to specify the option argument for '--with'\?\nTo specify an option argument starting with a dash use '--with=-EXAMPLE'\./);
+    parseArgs({ args: passedArgs, options, strict: true });
+  }, /Did you forget to specify the option argument for '--with'\?\nTo specify an option argument starting with a dash use '--with=-XYZ'\./);
   t.end();
 });
